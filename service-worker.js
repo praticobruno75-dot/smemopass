d6934297f385152e4b92f6833928309c695686f1
praticobruno75-dot <praticobruno75@gmail.com>
2026-01-17 19:44:20 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // SMEMOPASS Service Worker
-// Version 17.0.0 - Fixed chrome-extension errors
+// Version 19.0.0 - Google Drive Sync Integration
 
-const CACHE_NAME = 'smemopass-v17';
+const CACHE_NAME = 'smemopass-v19';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
